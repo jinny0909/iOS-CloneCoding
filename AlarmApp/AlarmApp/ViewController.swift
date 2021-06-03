4fb752eddb615b59c68f95293f308c98df741a22
Kyoungjin Lim <klim30@jhu.edu>
2021-06-03 16:31:25 +0900

    project init

---
+++ AlarmApp/AlarmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlarmApp
+//
+//  Created by kyoungjin on 2021/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
